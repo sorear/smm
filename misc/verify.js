503688c36045318b673a16b1212125df8e94390c
Stefan O'Rear <stefanor@cox.net>
2015-06-20 14:07:19 -0700

    let verify.js check the $t comment

--- misc/verify.js
+++ misc/verify.js
@@ -1,37 +1,29 @@
-var write = function (str) { process.stdout.write(str.toString(), 'utf8'); };
+function write(str) { process.stdout.write(str.toString(), 'utf8'); };
 
-var MMOM = require('../lib/MMOM.js');
-require('../lib/Scoper.js');
-require('../lib/Verifier.js');
-require('../lib/Parser.js');
-require('../lib/ConsoleErrorFormatter.js');
+import { parseSync } from '../lib/MMOM.js';
+import '../lib/Scoper.js';
+import '../lib/Verifier.js';
+import '../lib/Parser.js';
+import '../lib/ConsoleErrorFormatter.js';
+import '../lib/Metadata.js';
 
 function time(why,f) {
-    var time_1 = Date.now();
+    let time_1 = Date.now();
     f();
-    var time_2 = Date.now();
+    let time_2 = Date.now();
     write(`${why} ${time_2 - time_1} ms\n`);
 }
 
-var db;
+let db;
 
-time('parse', function () {
-    db = MMOM.parseSync(process.argv[2], function (s) { return require('fs').readFileSync(s, 'utf8'); });
-    db.scanner.errors.forEach(function (e) { write(e.toConsoleString()); });
+time('parse', () => {
+    db = parseSync(process.argv[2], s => require('fs').readFileSync(s, 'utf8'));
+    db.scanner.errors.forEach(e => write(e.toConsoleString()));
 });
-time('scope', function () {
-    db.scoper.allErrors.forEach(function (errs, s) {
-        errs.forEach(function (e) { write(e.toConsoleString()); });
-    });
-});
-time('parser', function () {
-    //db.parser._buildParser();
-    db.parser.allErrors.forEach(function (errs, s) {
-        errs.forEach(function (e) { write(e.toConsoleString()); });
-    });
-});
-time('verify', function () {
-    db.verifier.allErrors.forEach(function (errs, s) {
-        errs.forEach(function (e) { write(e.toConsoleString()); });
+['scoper','parser','metadata','verifier'].forEach(stage => {
+    time(stage, () => {
+        db[stage].allErrors.forEach(errs => {
+            errs.forEach(e => write(e.toConsoleString()));
+        });
     });
 });
